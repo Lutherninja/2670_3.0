5ce78e07fac71b42ba71f79cf7b1b48a4b21a2f6
Unknown <Luther.ninja@gmail.com>
2018-10-17 14:30:44 -0600

    Figuring out JumpyBoi

---
+++ PaperPlane/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.Experimental.PlayerLoop;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.XR.WSA.WebCam;
+
+[RequireComponent(typeof(Scene))]
+
+public class DeathZone : MonoBehaviour
+{
+   public void OnTriggerEnter(Collider other)
+   {
+       
+   }
+}
---
+++ PaperPlane/Assets/Scripts/MovePatternNotGrounded.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+[CreateAssetMenu(fileName = "MovePatternNotGrounded")]
+public class MovePatternNotGrounded : MovePattern {
+
+	public override void Invoke(CharacterController controller, Transform transform)
+	
+	
+	
+	{
+	
+		Move(controller);
+			Move(transform);
+		
+		
+	}
+	
+}
+
